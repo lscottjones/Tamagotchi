bff298246de3ace78c6a1e039232eb14f6b0c687
Lawrence <lscottjones440@gmail.com>
2018-01-18 01:12:32 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+ $("#playbutton").click(function(){
+   play();
+});
+
+});
+
+ $("#sleepbutton").click(function(){
+   sleep();
+});
+
+ $("#feedbutton").click(function(){
+   feed();
+})
+ $("#petname").click(function(){
+   alert("Thank you for the information");
+});
---
+++ pet.js
@@ -0,0 +1,57 @@
+//function to create the pet object
+var createPet = function (){
+  var pet = {
+    food: 5,
+    energy: 5,
+    happiness: 5,
+ }
+  //alert (pet)
+ };
+//global variables
+var pet = createPet()
+alert(pet)
+
+//play function
+var play = function (){
+    pet.food = pet.food -1
+    pet.energy = pet.energy -1
+    pet.happiness = pet.happiness +3
+  alert('You played with your pet.')
+  var newPet = pet
+  alert(newPet)
+  isAlive()
+}
+
+//feed function
+var feed = function (){
+    pet.food = pet.food +2
+    pet.energy = pet.energy -1
+    pet.happiness = pet.happiness +1
+  con('You fed your pet.')
+    var newPet = pet
+  alert(newPet)
+  isAlive()
+}
+
+//sleep function
+var sleep = function (){
+get_random = function (list) {
+  return list[Math.floor((Math.random()*list.length))];
+} 
+
+get_random([-2,-3,-5])
+var thisfood = get_random
+    pet.food = thisfood +4
+    pet.energy = pet.energy +3
+    pet.happiness = pet.happiness +2
+  alert('Your pet has slept.') 
+  var newPet = pet
+  alert(newPet)
+  isAlive()
+}
+
+
+createPet()
+
+
+
